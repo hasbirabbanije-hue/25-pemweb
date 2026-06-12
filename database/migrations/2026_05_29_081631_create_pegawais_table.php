a6ad311b876447f1d0880d1ba08931f1dc98489d
hasbi <hasbirabbanije@gmail.com>
2026-06-12 08:58:48 +0700

    upload pertemuan-13

---
+++ database/migrations/2026_05_29_081631_create_pegawais_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pegawais', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
+            $table->string('nim');
+            $table->string('nama');
+            $table->string('gender');
+            $table->foreignId('divisi_id')->constrained('divisis')->cascadeOnDelete();
+            $table->foreignId('jabatan_id')->constrained('jabatans')->cascadeOnDelete();
+            $table->string('tmp_lahir');
+            $table->date('tgl_lahir');
+            $table->string('hp');
+            $table->text('alamat');
+            $table->string('foto')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pegawais');
+    }
+};
